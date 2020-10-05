8e74b77d9034642475307330edf7d369f75f565a
Ross <ross.srk@gmail.com>
2020-10-05 12:32:23 -0700

    Small case sensitivity bugfix

--- src/rt-api/get-contacts.ts
+++ src/rt-api/get-contacts.ts
@@ -1,5 +1,5 @@
 import { REDTAIL_API_URL } from "@shared/constants";
-import { createRtApiConfig } from "@shared/utils/createRtApiconfig";
+import { createRtApiConfig } from "@shared/utils/createRtApiConfig";
 import Axios from "axios";
 
 export const getContactsByPage = (userKey: string, pageNumber: number) => {
