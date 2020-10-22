93e680150355c0a20acd0dee3ae2b3a8185813a0
Bryan <bryan87reed@gmail.com>
2020-10-22 16:56:07 -0700

    Adds production check when loading env vars

--- src/LoadEnv.ts
+++ src/LoadEnv.ts
@@ -12,10 +12,13 @@ const options = commandLineArgs([
 ]);
 
 // Set the env file
-const result2 = dotenv.config({
-    path: `./env/${options.env}.env`,
-});
+const env = process.env.NODE_ENV;
+if (env !== 'production') {
+    const result2 = dotenv.config({
+        path: `./env/${options.env}.env`,
+    });
 
-if (result2.error) {
-    throw result2.error;
+    if (result2.error) {
+        throw result2.error;
+    }
 }
