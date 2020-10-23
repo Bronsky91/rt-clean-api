3d677a59d778144a3862cfc55ddc2dcb17f649a9
Bryan <bryan87reed@gmail.com>
2020-10-22 18:45:31 -0700

    prod port fix

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import "./LoadEnv"; // Must be the first import
 import app from "./Server"
 import logger from "./shared/Logger";
 
+const env = process.env.NODE_ENV;
 // Start the server
-const port = process.env.PORT || 8000;
+const port = env !== 'production' ? 8000 : process.env.PORT;
 app.listen(port, () => {
   logger.info("Express server started on port: " + port);
 });
