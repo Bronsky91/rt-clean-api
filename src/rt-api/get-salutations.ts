a71c03c1cdd20a63b87a367db48c92e0fcf9dbec
Ross <ross.srk@gmail.com>
2020-10-29 19:36:16 -0700

    Fixes TWAPI salutation fetching

--- src/rt-api/get-salutations.ts
+++ src/rt-api/get-salutations.ts
@@ -1,15 +1,40 @@
-import Axios from 'axios';
-import { RedtailList } from '../interfaces/redtail-settings.interface';
-import { REDTAIL_TWAPI_URL } from '../shared/constants';
-import { createRtApiConfig } from '../shared/utils/createRtApiConfig';
+import Axios, { AxiosResponse } from "axios";
+import { RedtailList } from "../interfaces/redtail-settings.interface";
+import { REDTAIL_TWAPI_URL } from "../shared/constants";
+import logger from "../shared/Logger";
+import { createRtApiConfig } from "../shared/utils/createRtApiConfig";
 
 export const getSalutations = async (
   userKey: string
 ): Promise<RedtailList[]> => {
-  const result = await Axios.get(
-    REDTAIL_TWAPI_URL + "/lists/contact_salutations",
+  const pageOneRes: AxiosResponse<any> = await Axios.get(
+    REDTAIL_TWAPI_URL + "/lists/contact_salutations?page=1",
     createRtApiConfig(userKey)
   );
-  const salutations: RedtailList[] = result.data.contact_salutations.map((s: RedtailList) => ({id: s.name, name: s.name}))
-  return salutations
+  let salutations: RedtailList[] = pageOneRes.data.contact_salutations.map(
+    (s: RedtailList) => ({ id: s.id, name: s.name })
+  );
+
+  const totalPages = Array(pageOneRes.data.meta.total_pages - 2 + 1)
+    .fill(0)
+    .map((_, idx) => 2 + idx);
+  const salutationPromises = totalPages.map(async (page) => {
+    return Axios.get(
+      REDTAIL_TWAPI_URL + `/lists/contact_salutations?page=${page}`,
+      createRtApiConfig(userKey)
+    ).then((res) => {
+      const sal: RedtailList[] = res.data.contact_salutations.map(
+        (s: RedtailList) => ({ id: s.id, name: s.name })
+      );
+      return sal;
+    });
+  });
+
+  const nextSalutations = await Promise.all(salutationPromises);
+  const flatSalutations = nextSalutations.reduce(
+    (acc, val) => acc.concat(val),
+    []
+  );
+  salutations = [...salutations, ...flatSalutations];
+  return salutations;
 };
